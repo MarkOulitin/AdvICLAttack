b0a24576a0c3d434cbab5d9349f8210e63c1f13b
liadgiladi <liadgiladi@gmail.com>
2023-07-14 00:01:22 +0300

    added initial logic for icl attack

--- attack_utils.py
+++ attack_utils.py
@@ -1,4 +1,175 @@
+import numpy as np
+from textattack import Attack
+from textattack.attack_recipes import TextBuggerLi2018
+from textattack.constraints.pre_transformation import RepeatModification, StopwordModification
+from textattack.constraints.semantics.sentence_encoders import UniversalSentenceEncoder
+from textattack.goal_function_results import GoalFunctionResultStatus
+from textattack.goal_functions import UntargetedClassification
 from textattack.models.wrappers import ModelWrapper
+from textattack.search_methods import GreedyWordSwapWIR, SearchMethod
+from textattack.shared.validators import transformation_consists_of_word_swaps_and_deletions
+from textattack.transformations import CompositeTransformation, WordSwapRandomCharacterInsertion, \
+    WordSwapRandomCharacterDeletion, WordSwapNeighboringCharacterSwap, WordSwapHomoglyphSwap, WordSwapEmbedding
+
+
+class ICLAttack(TextBuggerLi2018):
+    @staticmethod
+    def build(model_wrapper):
+        #
+        #  we propose five bug generation methods for TEXTBUGGER:
+        #
+        transformation = CompositeTransformation(
+            [
+                # (1) Insert: Insert a space into the word.
+                # Generally, words are segmented by spaces in English. Therefore,
+                # we can deceive classifiers by inserting spaces into words.
+                WordSwapRandomCharacterInsertion(
+                    random_one=True,
+                    letters_to_insert=" ",
+                    skip_first_char=True,
+                    skip_last_char=True,
+                ),
+                # (2) Delete: Delete a random character of the word except for the first
+                # and the last character.
+                WordSwapRandomCharacterDeletion(
+                    random_one=True, skip_first_char=True, skip_last_char=True
+                ),
+                # (3) Swap: Swap random two adjacent letters in the word but do not
+                # alter the first or last letter. This is a common occurrence when
+                # typing quickly and is easy to implement.
+                WordSwapNeighboringCharacterSwap(
+                    random_one=True, skip_first_char=True, skip_last_char=True
+                ),
+                # (4) Substitute-C (Sub-C): Replace characters with visually similar
+                # characters (e.g., replacing “o” with “0”, “l” with “1”, “a” with “@”)
+                # or adjacent characters in the keyboard (e.g., replacing “m” with “n”).
+                WordSwapHomoglyphSwap(),
+                # (5) Substitute-W
+                # (Sub-W): Replace a word with its topk nearest neighbors in a
+                # context-aware word vector space. Specifically, we use the pre-trained
+                # GloVe model [30] provided by Stanford for word embedding and set
+                # topk = 5 in the experiment.
+                WordSwapEmbedding(max_candidates=5),
+            ]
+        )
+
+        constraints = [RepeatModification(), StopwordModification()]
+        # In our experiment, we first use the Universal Sentence
+        # Encoder [7], a model trained on a number of natural language
+        # prediction tasks that require modeling the meaning of word
+        # sequences, to encode sentences into high dimensional vectors.
+        # Then, we use the cosine similarity to measure the semantic
+        # similarity between original texts and adversarial texts.
+        # ... "Furthermore, the semantic similarity threshold \eps is set
+        # as 0.8 to guarantee a good trade-off between quality and
+        # strength of the generated adversarial text."
+        constraints.append(UniversalSentenceEncoder(threshold=0.8))
+        #
+        # Goal is untargeted classification
+        #
+        goal_function = ICLUntargetedClassification(model_wrapper)
+        #
+        # Greedily swap words with "Word Importance Ranking".
+        #
+        search_method = GreedyWordSwapWIR(wir_method="delete")
+
+        return Attack(goal_function, constraints, transformation, search_method)
+
+    def _attack(self, initial_result):
+        pass
+
+    def attack(self, example, ground_truth_output):
+        assert isinstance(
+            ground_truth_output, (int, str)
+        ), "`ground_truth_output` must either be `str` or `int`."
+
+        goal_function_result, _ = self.goal_function.init_attack_example(
+            example, ground_truth_output
+        )
+        if goal_function_result.goal_status == GoalFunctionResultStatus.SKIPPED:
+            return SkippedAttackResult(goal_function_result)
+        else:
+            result = self._attack(goal_function_result)
+            return result
+
+
+class ICLUntargetedClassification(UntargetedClassification):
+    pass
+
+
+class ICLGreedyWordSwapWIR(SearchMethod):
+    def _get_index_order(self, initial_text):
+        """Returns word indices of ``initial_text`` in descending order of
+        importance."""
+
+        len_text, indices_to_order = self.get_indices_to_order(initial_text)
+
+        leave_one_texts = [
+            initial_text.delete_word_at_index(i) for i in indices_to_order
+        ]
+        leave_one_results, search_over = self.get_goal_results(leave_one_texts)
+        index_scores = np.array([result.score for result in leave_one_results])
+
+        index_order = np.array(indices_to_order)[(-index_scores).argsort()]
+
+        return index_order, search_over
+
+    def perform_search(self, initial_result):
+        attacked_text = initial_result.attacked_text
+
+        # Sort words by order of importance
+        index_order, search_over = self._get_index_order(attacked_text)
+        i = 0
+        cur_result = initial_result
+        results = None
+        while i < len(index_order) and not search_over:
+            transformed_text_candidates = self.get_transformations(
+                cur_result.attacked_text,
+                original_text=initial_result.attacked_text,
+                indices_to_modify=[index_order[i]],
+            )
+            i += 1
+            if len(transformed_text_candidates) == 0:
+                continue
+            results, search_over = self.get_goal_results(transformed_text_candidates)
+            results = sorted(results, key=lambda x: -x.score)
+            # Skip swaps which don't improve the score
+            if results[0].score > cur_result.score:
+                cur_result = results[0]
+            else:
+                continue
+            # If we succeeded, return the index with best similarity.
+            if cur_result.goal_status == GoalFunctionResultStatus.SUCCEEDED:
+                best_result = cur_result
+                # @TODO: Use vectorwise operations
+                max_similarity = -float("inf")
+                for result in results:
+                    if result.goal_status != GoalFunctionResultStatus.SUCCEEDED:
+                        break
+                    candidate = result.attacked_text
+                    try:
+                        similarity_score = candidate.attack_attrs["similarity_score"]
+                    except KeyError:
+                        # If the attack was run without any similarity metrics,
+                        # candidates won't have a similarity score. In this
+                        # case, break and return the candidate that changed
+                        # the original score the most.
+                        break
+                    if similarity_score > max_similarity:
+                        max_similarity = similarity_score
+                        best_result = result
+                return best_result
+
+        return cur_result
+
+    def check_transformation_compatibility(self, transformation):
+        """Since it ranks words by their importance, GreedyWordSwapWIR is
+        limited to word swap and deletion transformations."""
+        return transformation_consists_of_word_swaps_and_deletions(transformation)
+
+    @property
+    def is_black_box(self):
+        return True
 
 
 class ICLModelWrapper(ModelWrapper):
