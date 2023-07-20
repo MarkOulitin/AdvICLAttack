cd7ec730cf3f3a96652ea6a9d2daa6e3d5e84dd2
liadgiladi <liadgiladi@gmail.com>
2023-07-20 14:18:20 +0300

    added all examples attack

---
+++ all_examples_attack.py
@@ -0,0 +1,222 @@
+from copy import deepcopy
+
+import numpy as np
+from textattack.attack_recipes import TextBuggerLi2018
+from textattack.attack_results import SkippedAttackResult
+from textattack.constraints.pre_transformation import RepeatModification, StopwordModification
+from textattack.constraints.semantics.sentence_encoders import UniversalSentenceEncoder
+from textattack.goal_function_results import GoalFunctionResultStatus
+from textattack.search_methods import SearchMethod
+from textattack.shared import AttackedText
+from textattack.shared.validators import transformation_consists_of_word_swaps_and_deletions
+from textattack.transformations import CompositeTransformation, WordSwapRandomCharacterInsertion, \
+    WordSwapRandomCharacterDeletion, WordSwapNeighboringCharacterSwap, WordSwapHomoglyphSwap, WordSwapEmbedding
+
+from attack_utils import ICLUntargetedClassification, ICLClassificationGoalFunctionResult
+from icl_input import ICLInput
+from icl_sample_selection import get_strategy
+
+
+class ICLAllExamplesAttack(TextBuggerLi2018):
+    @staticmethod
+    def build(model_wrapper):
+        #
+        #  we propose five bug generation methods for TEXTBUGGER:
+        #
+        transformation = CompositeTransformation(
+            [
+                # (1) Insert: Insert a space into the word.
+                # Generally, words are segmented by spaces in English. Therefore,
+                # we can deceive classifiers by inserting spaces into words.
+                WordSwapRandomCharacterInsertion(
+                    random_one=True,
+                    letters_to_insert=" ",
+                    skip_first_char=True,
+                    skip_last_char=True,
+                ),
+                # (2) Delete: Delete a random character of the word except for the first
+                # and the last character.
+                WordSwapRandomCharacterDeletion(
+                    random_one=True, skip_first_char=True, skip_last_char=True
+                ),
+                # (3) Swap: Swap random two adjacent letters in the word but do not
+                # alter the first or last letter. This is a common occurrence when
+                # typing quickly and is easy to implement.
+                WordSwapNeighboringCharacterSwap(
+                    random_one=True, skip_first_char=True, skip_last_char=True
+                ),
+                # (4) Substitute-C (Sub-C): Replace characters with visually similar
+                # characters (e.g., replacing “o” with “0”, “l” with “1”, “a” with “@”)
+                # or adjacent characters in the keyboard (e.g., replacing “m” with “n”).
+                WordSwapHomoglyphSwap(),
+                # (5) Substitute-W
+                # (Sub-W): Replace a word with its topk nearest neighbors in a
+                # context-aware word vector space. Specifically, we use the pre-trained
+                # GloVe model [30] provided by Stanford for word embedding and set
+                # topk = 5 in the experiment.
+                WordSwapEmbedding(max_candidates=5),
+            ]
+        )
+
+        constraints = [RepeatModification(), StopwordModification()]
+        # In our experiment, we first use the Universal Sentence
+        # Encoder [7], a model trained on a number of natural language
+        # prediction tasks that require modeling the meaning of word
+        # sequences, to encode sentences into high dimensional vectors.
+        # Then, we use the cosine similarity to measure the semantic
+        # similarity between original texts and adversarial texts.
+        # ... "Furthermore, the semantic similarity threshold \eps is set
+        # as 0.8 to guarantee a good trade-off between quality and
+        # strength of the generated adversarial text."
+        constraints.append(UniversalSentenceEncoder(threshold=0.8))
+        #
+        # Goal is ICL untargeted classification
+        #
+        goal_function = ICLUntargetedClassification(model_wrapper, use_cache=False)
+        #
+        # ICL Greedily swap words with "Word Importance Ranking".
+        #
+        search_method = ICLAllExamplesSearchMethod()
+
+        return ICLAllExamplesAttack(goal_function, constraints, transformation, search_method)
+
+    def attack(self, icl_input: ICLInput, ground_truth_output):
+        assert isinstance(
+            ground_truth_output, (int, str)
+        ), "`ground_truth_output` must either be `str` or `int`."
+
+        goal_function_result, _ = self.goal_function.init_attack_example(
+            icl_input, ground_truth_output
+        )
+
+        if goal_function_result.goal_status == GoalFunctionResultStatus.SKIPPED:
+            return SkippedAttackResult(goal_function_result)
+        else:
+            # default strategy, choose first icl example for the attack
+            icl_example_selector = get_strategy('first')
+            icl_example_selector.select_example_and_update_metadata_inplace(icl_input)
+
+            result = self._attack(goal_function_result)
+            return result
+
+
+class ICLAllExamplesSearchMethod(SearchMethod):
+    def __init__(self, example_perturbation_bounds: int = 3):
+        self.example_perturbation_bounds = example_perturbation_bounds
+
+        super().__init__()
+
+    def _get_index_order(self, initial_icl_input: ICLInput):
+        """Returns word indices of ``initial_text`` in descending order of
+        importance."""
+
+        initial_text = initial_icl_input.attacked_text
+        len_text, indices_to_order = self.get_indices_to_order(initial_text)
+
+        leave_one_texts = [
+            initial_text.delete_word_at_index(i) for i in indices_to_order
+        ]
+        leave_one_icl_inputs = [ICLInput(initial_icl_input.example_sentences,
+                                         initial_icl_input.example_labels,
+                                         initial_icl_input.test_sentence,
+                                         initial_icl_input.params,
+                                         initial_icl_input.pertubation_example_sentence_index,
+                                         leave_one_text) for leave_one_text in leave_one_texts]
+
+        leave_one_results, search_over = self.get_goal_results(leave_one_icl_inputs)
+        index_scores = np.array([result.score for result in leave_one_results])
+
+        index_order = np.array(indices_to_order)[(-index_scores).argsort()]
+
+        return index_order, search_over
+
+    def perform_search(self, initial_result: ICLClassificationGoalFunctionResult):
+        def update_attacked_text(result, example_index):
+            result.icl_input.pertubation_example_sentence_index = example_index
+            result.icl_input.attacked_text = AttackedText(result.icl_input.example_sentences[example_index])
+            assert result.icl_input.attacked_text.text == result.icl_input.example_sentences[example_index]
+
+        def override_example_sentence(result, example_index):
+            result.icl_input.example_sentences[example_index] = result.icl_input.attacked_text.text
+
+        icl_input = initial_result.icl_input
+
+        cur_result = deepcopy(initial_result)
+
+        for example_index in range(len(icl_input.example_sentences)):
+            # update attacked text
+            update_attacked_text(cur_result, example_index)
+
+            # Sort words by order of importance of given example index
+            index_order, search_over = self._get_index_order(cur_result.icl_input)
+            i = 0
+
+            index_order = index_order[:self.example_perturbation_bounds]
+            while i < len(index_order) and not search_over:
+                transformed_text_candidates = self.get_transformations(
+                    cur_result.attacked_text,
+                    original_text=initial_result.attacked_text,
+                    indices_to_modify=[index_order[i]],
+                )
+
+                i += 1
+                if len(transformed_text_candidates) == 0:
+                    continue
+                transformed_icl_candidates = [ICLInput(icl_input.example_sentences,
+                                                       icl_input.example_labels,
+                                                       icl_input.test_sentence,
+                                                       icl_input.params,
+                                                       icl_input.pertubation_example_sentence_index,
+                                                       transformed_text_candidate) for transformed_text_candidate in
+                                              transformed_text_candidates]
+
+                results, search_over = self.get_goal_results(transformed_icl_candidates)
+                results = sorted(results, key=lambda x: -x.score)
+
+                # Skip swaps which don't improve the score
+                if results[0].score > cur_result.score:
+                    cur_result = results[0]
+                    # override example sentence
+                    override_example_sentence(cur_result, example_index)
+                else:
+                    continue
+                # If we succeeded, return the index with best similarity.
+                if cur_result.goal_status == GoalFunctionResultStatus.SUCCEEDED:
+                    best_result = cur_result
+                    # @TODO: Use vectorwise operations
+                    max_similarity = -float("inf")
+                    for result in results:
+                        if result.goal_status != GoalFunctionResultStatus.SUCCEEDED:
+                            break
+                        candidate = result.icl_input.attacked_text
+                        try:
+                            similarity_score = candidate.attack_attrs["similarity_score"]
+                        except KeyError:
+                            # If the attack was run without any similarity metrics,
+                            # candidates won't have a similarity score. In this
+                            # case, break and return the candidate that changed
+                            # the original score the most.
+                            break
+                        if similarity_score > max_similarity:
+                            max_similarity = similarity_score
+                            best_result = result
+
+                    # override example sentence
+                    override_example_sentence(cur_result, example_index)
+                    return best_result
+
+                # override example sentence
+                override_example_sentence(cur_result, example_index)
+
+        # override example sentence
+        override_example_sentence(cur_result, example_index)
+        return cur_result
+
+    def check_transformation_compatibility(self, transformation):
+        """Since it ranks words by their importance, GreedyWordSwapWIR is
+        limited to word swap and deletion transformations."""
+        return transformation_consists_of_word_swaps_and_deletions(transformation)
+
+    @property
+    def is_black_box(self):
+        return True
---
+++ run_on_all_examples.py
@@ -0,0 +1,208 @@
+import argparse
+from copy import deepcopy
+
+import numpy as np
+import textattack
+import torch
+
+from all_examples_attack import ICLAllExamplesAttack
+from attack_dataset import ICLDataset
+from attack_utils import ICLModelWrapper
+from data_utils import load_dataset
+from icl_attaker import ICLAttacker
+from llm_setups import setup_llama_hf
+from llm_utils import create_completion
+from utils import load_results, random_sampling
+
+
+def main(models: list[str],
+         datasets: list[str],
+         seeds: list[int],
+         num_few_shots: list[int],
+         subsample_test_set: int,
+         api_num_logprob: int,
+         use_saved_results: bool,
+         batch_size: int):
+    """
+    Run experiment or load past results, print accuracy
+    """
+    default_params = {
+        'conditioned_on_correct_classes': True,
+        'subsample_test_set': subsample_test_set,
+        'api_num_logprob': api_num_logprob,
+        'batch_size': batch_size
+    }
+
+    # list of all experiment parameters to run
+    all_params = []
+    for model in models:
+        for dataset in datasets:
+            for num_shots in num_few_shots:
+                for seed in seeds:
+                    p = deepcopy(default_params)
+                    p['model'] = model
+                    p['dataset'] = dataset
+                    p['seed'] = seed
+                    p['num_shots'] = num_shots
+                    p['expr_name'] = f"all_examples_{p['dataset']}_{p['model']}_{p['num_shots']}_shot_{repr(p['subsample_test_set'])}_subsample_{p['seed']}_seed"
+                    all_params.append(p)
+
+    # query the model and save the responses
+    if use_saved_results:
+        load_results(all_params)
+    else:
+        run_experiments(all_params)
+
+
+def run_experiments(params_list):
+    """
+    Run the experiments and save its responses and the rest of configs
+    """
+
+    device = "cpu" if not torch.cuda.is_available() else "cuda"
+    print(f"device={device}")
+    print(f"# of gpus={torch.cuda.device_count()}")
+
+    for param_index, params in enumerate(params_list):
+        experiment_name = params['expr_name']
+        print("\nExperiment name:", experiment_name)
+
+        # load data
+        example_sentences, example_labels, test_sentences, test_labels = load_dataset(params)
+        print(f"dataset {params['dataset']} stats:")
+        print(f"size of examples pool: {len(example_labels)}")
+        print(f"num of test sentences: {len(test_labels)}")
+
+        # llm setup
+        llm = setup_llama_hf(device, params['inv_label_dict'])
+
+        # sanity check
+        experiment_sanity_check(params, device, llm)
+
+        # set seed
+        np.random.seed(params['seed'])
+
+        # sample test set
+        if params['subsample_test_set'] is None:
+            print(f"selecting full test set ({len(test_labels)} examples)")
+            test_sentences, test_labels = test_sentences, test_labels
+        else:
+            print(f"selecting {params['subsample_test_set']} subsample of test set")
+
+            test_sentences, test_labels = random_sampling(test_sentences,
+                                                          test_labels,
+                                                          params['subsample_test_set'])
+
+        attack, attack_dataset, attack_args = setup_attack_success_rate_experiment(llm,
+                                                                                   device,
+                                                                                   test_sentences,
+                                                                                   test_labels,
+                                                                                   example_sentences,
+                                                                                   example_labels,
+                                                                                   experiment_name,
+                                                                                   params)
+
+        attacker = ICLAttacker(experiment_name, attack, attack_dataset, attack_args)
+        attacker.attack_dataset()
+
+
+def setup_attack_success_rate_experiment(llm,
+                                         device,
+                                         test_sentences,
+                                         test_labels,
+                                         example_sentences,
+                                         example_labels,
+                                         experiment_name,
+                                         params):
+    icl_model_wrapper = ICLModelWrapper(llm, device)
+    attack = ICLAllExamplesAttack.build(icl_model_wrapper)
+
+    attack_dataset = ICLDataset(test_sentences, test_labels, example_sentences, example_labels,
+                                params['num_shots'], params)
+
+    attack_args = textattack.AttackArgs(
+        num_examples=len(attack_dataset),
+        log_to_txt=f"./log_{experiment_name}.txt",
+        log_to_csv=f"./log_{experiment_name}.csv",
+        log_summary_to_json=f"./attack_summary_log_{experiment_name}.json",
+        csv_coloring_style="plain",
+        checkpoint_interval=5,
+        checkpoint_dir="./checkpoints",
+        disable_stdout=True,
+        # query_budget=60,  # TODO remove
+    )
+
+    return attack, attack_dataset, attack_args
+
+
+def experiment_sanity_check(params: dict, device: str, llm):
+    """Sanity check the experiment"""
+
+    assert params['num_tokens_to_predict'] == 1
+
+    model_name = params['model']
+
+    # for classification, make sure that all of the class names are one word.
+    for key, label_names in params['label_dict'].items():
+        for label_id, label_name in enumerate(label_names):
+            prompt = label_name
+
+            if model_name == 'llama_cpp':
+                response = create_completion(prompt, params['inv_label_dict'], 1,
+                                             model_name, device, llm, echo=True, num_logprobs=1)  # set echo to True
+                first_token_of_label_name = response['choices'][0]['logprobs']['tokens'][0][1:]  # without the prefix space
+
+                if first_token_of_label_name != label_name:
+                    print('label name is more than 1 token', label_name)
+                    assert False
+
+            elif model_name == 'llama_hf':
+                label_to_token = llm.label_to_token
+                assert label_name in label_to_token
+                # response, probs = create_completion(prompt, params['inv_label_dict'], 1,
+                #                                     model_name, device, echo=True, num_logprobs=1,
+                #                                     llm=llm)  # set echo to True
+                # first_token_of_label_name = response[2]  # take the third token, first tokens are "<s> "
+
+            else:
+                raise NotImplementedError
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # required arguments
+    parser.add_argument('--models', dest='models', action='store', required=True,
+                        help='name of model(s), e.g., Llama')
+    parser.add_argument('--datasets', dest='datasets', action='store', required=True,
+                        help='name of dataset(s), e.g., sst2')
+    parser.add_argument('--seeds', dest='seeds', action='store', required=True,
+                        help='seeds for the training set')
+    parser.add_argument('--num_few_shots', dest='num_few_shots', action='store', required=True,
+                        help='num training examples to use')
+    # other arguments
+    parser.add_argument('--subsample_test_set', dest='subsample_test_set', action='store', required=False, type=int,
+                        default=None, help='size of test set to use to speed up eval. None means using all test set')
+    parser.add_argument('--api_num_logprob', dest='api_num_logprob', action='store', required=False, type=int,
+                        default=100,
+                        help='number of top tokens to ask for when querying the model. Capped at 100 for OpenAI GPT-3 API')
+    parser.add_argument('--batch_size', dest='batch_size', action='store', required=False, type=int, default=None,
+                        help='batch size for model queries. For OpenAI API, capped at 20. For local running, set this to max out your GPU memory.')
+    # flags
+    parser.add_argument('--use_saved_results', dest='use_saved_results', action='store_const', const=True, default=False,
+                        help='whether to load the results from pickle files and not run the model')
+
+    args = parser.parse_args()
+    args = vars(args)
+
+    def convert_to_list(items, is_int=False):
+        if is_int:
+            return [int(s.strip()) for s in items.split(",")]
+        else:
+            return [s.strip() for s in items.split(",")]
+
+    args['models'] = convert_to_list(args['models'])
+    args['datasets'] = convert_to_list(args['datasets'])
+    args['seeds'] = convert_to_list(args['seeds'], is_int=True)
+    args['num_few_shots'] = convert_to_list(args['num_few_shots'], is_int=True)
+
+    main(**args)
