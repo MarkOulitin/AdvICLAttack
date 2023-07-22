ca24070c5e2b6bec5702f651bdb65f599d0e519a
Mark Oulitin <fragoric@gmail.com>
2023-07-22 22:11:02 +0300

    Added sentence selection

--- icl_sample_selection.py
+++ icl_sample_selection.py
@@ -61,8 +61,101 @@ class GreedyExampleSelection(ExampleSelectionStrategy):
         icl_input.pertubation_example_sentence_index = most_important_example_index
         assert icl_input.attacked_text.text == icl_input.example_sentences[most_important_example_index]
 
+# class GreedyExampleAndSentenceSelection(ExampleSelectionStrategy):
+#     def __init__(self, goal_function: utils.ICLUntargetedClassification) -> None:
+#         super().__init__()
+#         self._goal_function: utils.ICLUntargetedClassification = goal_function
+#         self.example_selection: GreedyExampleSelection = GreedyExampleSelection(goal_function)
 
-class GreedyExampleSentenceSelection(ExampleSelectionStrategy):
+#     def select_example(self, sample: utils.ICLInput):
+#         """select example and update in place"""
+#         self.example_selection.select_example_and_update_metadata_inplace(sample)
+    
+#     def split_text_to_sentences(self, text):
+#         import nltk
+#         nltk.download('punkt')  # Download the Punkt tokenizer data if not available
+
+#         # Preprocess the text to handle emojis and punctuation marks
+#         text = re.sub(r'([^\s\w.?!]|_)+', ' ', text)
+        
+#         # Use the NLTK tokenizer to split the text into sentences
+#         sentences = nltk.sent_tokenize(text)
+#         return sentences
+    
+#     @abstractmethod
+#     def select_example_and_update_metadata_inplace(self, sample: utils.ICLInput):
+#         pass
+    
+# class GreedyExampleAndFirstSentenceSelection(GreedyExampleAndSentenceSelection):
+
+#     def __init__(self, goal_function: utils.ICLUntargetedClassification) -> None:
+#         super().__init__(goal_function)
+    
+#     def select_example_and_update_metadata_inplace(self, sample: utils.ICLInput):
+#         """
+#         Select the most important example, then select first sentence in the example
+#         """
+#         self.select_example(sample)
+#         sentences = self.split_text_to_sentences(sample.example_sentences[0])
+#         if len(sentences) == 1:
+#             # we selected the most important  example but by chance it contains only one sentence
+#             return
+#         selected_sentence_index = 0
+#         sample.attacked_text = AttackedText(sentences[selected_sentence_index])
+#         assert sample.attacked_text.text == sentences[selected_sentence_index]
+
+
+# class GreedyExampleAndRandomSentenceSelection(GreedyExampleAndSentenceSelection):
+
+#     def __init__(self, goal_function: utils.ICLUntargetedClassification) -> None:
+#         super().__init__(goal_function)
+    
+#     def select_example_and_update_metadata_inplace(self, sample: utils.ICLInput):
+#         """
+#         Select the most important example, then select random sentence in the example
+#         """
+#         self.select_example(sample)
+#         sentences = self.split_text_to_sentences(sample.example_sentences[0])
+#         if len(sentences) == 1:
+#             # we selected the most important  example but by chance it contains only one sentence
+#             return
+#         selected_sentence_index = np.random.randint(0, len(sentences))
+#         sample.attacked_text = AttackedText(sentences[selected_sentence_index])
+#         assert sample.attacked_text.text == sentences[selected_sentence_index]
+
+# class GreedyExampleAndSentenceSelection(GreedyExampleAndSentenceSelection):
+
+#     def __init__(self, goal_function: utils.ICLUntargetedClassification) -> None:
+#         super().__init__(goal_function)
+    
+#     def select_example_and_update_metadata_inplace(self, sample: utils.ICLInput):
+#         """
+#         Select the most important example, then the most important sentence in the example
+#         """
+#         self.select_example(sample)
+#         sentences = self.split_text_to_sentences(sample.example_sentences[0])
+#         if len(sentences) == 1:
+#             # we selected the most important  example but by chance it contains only one sentence
+#             return
+#         masked_one_examples = [
+#             utils.ICLInput(
+#                 example_sentences=[sentence],
+#                 example_labels=sample.example_labels,
+#                 test_sentence=sample.test_sentence,
+#                 params=sample.params,
+#                 pertubation_example_sentence_index = sample.pertubation_example_sentence_index,
+#                 attacked_text = AttackedText(sentence),
+#             )
+#             for sentence in sentences
+#         ]
+#         masked_one_examples_results, search_over = self._goal_function.get_results(masked_one_examples)
+#         index_scores = np.array([result.score for result in masked_one_examples_results])  # scores are 1 - prob
+#         most_important_sentence_index = np.argmax(index_scores)
+
+#         sample.attacked_text = AttackedText(sentences[most_important_sentence_index])
+#         assert sample.attacked_text.text == sentences[most_important_sentence_index]
+
+class ExampleImportannceOrder(ExampleSelectionStrategy):
 
     def __init__(self, goal_function: utils.ICLUntargetedClassification) -> None:
         super().__init__()
@@ -70,48 +163,10 @@ class GreedyExampleSentenceSelection(ExampleSelectionStrategy):
         self.example_selection: GreedyExampleSelection = GreedyExampleSelection(goal_function)
 
     def select_example_and_update_metadata_inplace(self, sample: utils.ICLInput):
-        if len(sample.example_sentences) != len(sample.example_labels):
-            raise Exception('Got sample with unequal amount of examples and labels')
-        if len(sample.example_sentences) == 0:
-            raise Exception('Got sample without examples and labels')
-        if len(sample.example_sentences) == 1:
-            raise Exception('Got sample with one example and label')
-
-        self.example_selection.select_example_and_update_metadata_inplace(sample)
-        sentences = self._split_text_to_sentences(sample.example_sentences[0])
-        if len(sentences) == 1:
-            # we selected the most example but by chance it contains only one sentence
-            return
-        min_score = self._model([sample])[0] # TODO
-        most_imporatant_sentence_index = -1
-        for i in range(len(sentences)):
-            masked_sample: utils.ICLInput = self._mask_sentence_of_sample(sample, sentences, i)
-            score = self._model([masked_sample])[0]
-            if score < min_score:
-                most_imporatant_sentence_index = i
-                min_score = score
-        sample.example_sentences = [sentences[most_imporatant_sentence_index]]
-    
-    def _mask_sentence_of_sample(self, sample: utils.ICLInput, sentences: list[str], sentence_index: int) -> utils.ICLInput:
-        return utils.ICLInput(
-            example_sentences=[' '.join([sentence for i, sentence in enumerate(sentences) if i != sentence_index])],
-            example_labels=sample.example_labels, 
-            test_sentence=sample.test_sentence,
-            params=sample.params,
-            pertubation_example_sentence_index=sample.pertubation_example_sentence_index,
-            attacked_text=sample.attacked_text
-        )
-    
-    def _split_text_to_sentences(self, text):
-        import nltk
-        nltk.download('punkt')  # Download the Punkt tokenizer data if not available
-
-        # Preprocess the text to handle emojis and punctuation marks
-        text = re.sub(r'([^\s\w.?!]|_)+', ' ', text)
-        
-        # Use the NLTK tokenizer to split the text into sentences
-        sentences = nltk.sent_tokenize(text)
-        return sentences
+        """
+        Reorder examples by importance order
+        """
+        # TODO
 
 
 class ExampleSelector:
@@ -136,12 +191,12 @@ def get_strategy(strategy_method: str, goal_function: utils.ICLUntargetedClassif
         strategy = FistExampleSelection()
     elif strategy_method == 'random':
         strategy = RandomExampleSelection()
-    elif strategy_method in ['greedy-example', 'greedy-example-sentence']:
+    elif strategy_method in ['greedy-example', 'greedy-example-and-sentence']:
         if goal_function is None:
             raise Exception('to use model dependent strategy goal_function must not be None')
         strategy = _get_model_dependent_strategy(strategy_method, goal_function)
     else:
-        raise Exception("got strategy that is not 'first', 'random', 'greedy-example' or 'greedy-example-sentence'")
+        raise Exception("got strategy that is not 'first', 'random', 'greedy-example' or 'greedy-example-and-sentence'")
 
     example_selector = ExampleSelector(strategy)
     return example_selector
@@ -150,7 +205,7 @@ def get_strategy(strategy_method: str, goal_function: utils.ICLUntargetedClassif
 def _get_model_dependent_strategy(strategy_method: str, goal_function: utils.ICLUntargetedClassification) -> ExampleSelectionStrategy:
     if strategy_method == 'greedy-example':
         return GreedyExampleSelection(goal_function)
-    elif strategy_method == 'greedy-example-sentence':
-        return GreedyExampleSentenceSelection(goal_function)
+    elif strategy_method == 'greedy-example-and-sentence':
+        return GreedyExampleAndSentenceSelection(goal_function)
     else:
-        raise Exception("got strategy that is not 'greedy-example' or 'greedy-example-sentence'")
+        raise Exception("got strategy that is not 'greedy-example' or 'greedy-example-and-sentence'")
